20c6a9ea63271c899121865d331ff57d4a92b9cb
Hleb Brazhynski <hleb200217@gmail.com>
2021-10-20 04:04:45 -0400

    funHW

---
+++ index.js
@@ -0,0 +1,67 @@
+function mySum(g,v){
+    return g+v;
+}
+
+console.log(mySum(5,6));
+
+function myAddFunction(g,v,b){
+    return g+v+b;
+}
+console.log(myAddFunction(5,6,7));
+
+function mySumAddFunction(g,v,b,e){
+    return g+v+b+e;
+}
+console.log(mySumAddFunction(5,6,7,8));
+
+// one through five ^
+
+var myBike = {
+    "make": "Suzuki",
+    "wheels": 2,
+    "lights": 4,
+    "colors": ["red", "white", "black"]
+
+};
+
+console.log(makeValue = myBike.make)
+console.log(wheelsValue = myBike.wheels)
+console.log(lightsValue = myBike.lights)
+console.log(colorsValue = myBike.colors[2])
+
+
+// six and seven ^
+
+
+console.log["make"]
+console.log["wheels"]
+console.log["lights"]
+console.log["colors"[1]]
+
+// eight ^
+
+
+myBike.speed = 177;
+
+console.log(speed = myBike)
+
+// nine ^
+
+
+var myBike = {
+    "make": "Yamaha",
+    "wheels": 2,
+    "speed": 177,
+    "colors": ["black", "white", "gold"]
+
+};
+
+console.log(makeValue = myBike.make)
+console.log(wheelsValue = myBike.wheels)
+console.log(lightsValue = myBike.speed)
+console.log(colorsValue = myBike.colors[0])
+
+
+
+
+
